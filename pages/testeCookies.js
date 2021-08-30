d8c5a741630f7dbbe88256dcbc3e468aa9920246
MarcelSussai <marcelsussaideoliveira@hotmail.com>
2021-08-30 18:57:56 -0300

    testando axios

---
+++ pages/testeCookies.js
@@ -0,0 +1,76 @@
+import { parseCookies, setCookie, destroyCookie } from 'nookies'
+import { useEffect, useState } from 'react';
+// import { io } from 'socket.io-client';
+import { axiosSimp } from '../components/services/axios';
+
+
+
+const sty01 = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const sty02 = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const TesteCookies = () => {
+  const [v01, setV01] = useState('')
+  const [v02, setV02] = useState({})
+  const [v03, setV03] = useState('')
+  const [v04, setV04] = useState('')
+
+  const [coo, setCoo] = useState({})
+
+  useEffect(() => {
+    const cookies = parseCookies()
+    console.log(cookies);
+    cookies.v01 ? setV01(cookies.v01) : ''
+  }, [])
+
+  const hdlV01 = () => {
+    setCookie(null, 'v01', 'teste02', {
+      maxAge: 30 * 24 * 60 * 60,
+      path: '/testeCookies',
+    })
+  }
+
+  const hdlV02 = async () => {
+    const result = await axiosSimp({
+      method: 'get',
+      url: '/users',
+    }).then(res => res.data)
+    console.log(result);
+  }
+
+  // const socket = io('http://localhost:3001', { path: '/' })
+  // socket.on('connect', () => console.log('[IO] - conectado => nova conexão com sucesso! - ', socket.id))
+
+
+  return (
+  <>
+    <div styles={sty01}>
+      <div styles={sty02}>
+        <p>{`Valor 01: ${v01}`}</p>
+        <p>{`Valor 01: ${v02}`}</p>
+        <p>{`Valor 01: ${v03}`}</p>
+        <p>{`Valor 01: ${v04}`}</p>
+      </div>
+      <div styles={sty02}>
+        <button onClick={hdlV01}> btn01 </button>
+        <button onClick={hdlV02}> btn02 </button>
+        <button> btn03 </button>
+        <button> btn04 </button>
+      </div>
+    </div>
+  </>
+);
+
+}
+
+
+export default TesteCookies;
