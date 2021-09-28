967568533956f8fb6642a25548baa3ce95463f50
MarcelSussai <marcelsussaideoliveira@hotmail.com>
2021-09-28 03:27:28 -0300

    criando paineis

--- components/services/axios.js
+++ components/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const dev = false
-export const url =  dev ? 'http://localhost:3001/' : 'https://app.getlogclub.com.br/'
+export const url = dev ? 'http://localhost:3001/' : 'https://app.getlogclub.com.br/'
 
 export const axiosSimp = axios.create({
   baseURL: url,
